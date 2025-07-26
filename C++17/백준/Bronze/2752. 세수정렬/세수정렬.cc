c007dcf1197db957af2302dda54835f973bbdddd
시은 <dawndrizzle02@gmail.com>
2025-07-26 15:13:20 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    int max, mid, min;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c) {
+        max = a;
+        mid = (b > c) ? b : c;
+        min = (b < c) ? b : c;
+    } else if (b > a && b > c) {
+        max = b;
+        mid = (a > c) ? a : c;
+        min = (a < c) ? a : c;
+    } else {
+        max = c;
+        mid = (a > b) ? a : b;
+        min = (a < b) ? a : b;
+    }
+
+    printf("%d %d %d\n", min, mid, max);
+    return 0;
+}
